221624a3c17a635f2fb4fa5f9261a0378436b600
kaustubh <kaustubhkj@gmail.com>
2024-06-26 00:45:03 +0530

    Squashed commit of the following:
    commit 7d89a7e5c418849c46a083a8f14c0156101549c1
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Wed Jun 26 00:44:10 2024 +0530
    
        schema registry code changes done
    
    commit 8da327162b48a1149b98d476c49eaaac0dd832f2
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Tue Jun 25 23:20:27 2024 +0530
    
        clean up
    
    commit 13badbd06231fbad66804fa6e789d131175621fd
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Tue Jun 25 17:22:53 2024 +0530
    
        smore sqlite stuff
    
    commit 27353891108cc8e203f03995e1acd7e05b608c43
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Tue Jun 25 16:56:06 2024 +0530
    
        initials of sqlite
    
    commit 728f8d02c31e77258545e8f6a66d50c722403655
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Tue Jun 25 16:29:24 2024 +0530
    
        switching to zap global
    
    commit 34af30aaacb17852e6b8a97f14ff928149036fd8
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Thu May 23 23:26:20 2024 +0530
    
        need to consumer and producer groups + a lot
    
    commit dc531a8e3f0922afb45957dd9ecef626e3e370e0
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Wed May 22 20:52:40 2024 +0530
    
        logging stuff
    
    commit 3936c264d6cc04223e875db68e1d53584af498d7
    Author: kaustubh <kaustubhkj@gmail.com>
    Date:   Wed May 22 15:08:03 2024 +0530
    
        intial golang imports

--- vanilla/golang-interaction/management/sql_queries.go
+++ vanilla/golang-interaction/management/sql_queries.go
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS avro_store(id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL, 
 		schema BLOB NOT NULL, 
 		version int NOT NULL,
-		created_at DATETIME DEFAULT (now()),
+		created_at DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT unique_schema_version UNIQUE (name, version)
 	);
 CREATE UNIQUE INDEX IF NOT EXISTS name_lookup_index ON avro_store (name);
@@ -16,4 +16,4 @@ const SchemaFetchQuery string = `SELECT schema FROM avro_store WHERE name = ? AN
 const SchemaDeleteQuery string = `DELETE * FROM avro_store WHERE name = ? AND version = ?`
 const SchemaBulkDeleteQuery string = `DELETE * FROM avro_store WHERE name = ?`
 const SchemaInsertQuery string = `INSERT INTO avro_store(name, version, schema) VALUES(?,?,?)`
-const SchemaUpdateQuery string = `UPDATE avro_store SET schema = ? WHERE name = ?`
+const SchemaUpdateQuery string = `UPDATE avro_store SET schema = ? WHERE name = ? AND version = ?`
